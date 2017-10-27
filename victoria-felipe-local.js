7c8499cba69705973d0e5487cea7be324b748ffa
Victória Felipe Damascena Souza <victoriadamascena222@gmail.com>
2017-10-27 10:55:41 -0200

    trab

---
+++ victoria-felipe-local.js
@@ -0,0 +1,157 @@
+db.alunos.insert( {
+   name: "Layla Teodora",
+   location: { type: "Point", coordinates: [-43.3179687 , -15.8198619 ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Mercia",
+   location: { type: "Point", coordinates: [ -43.0685685,-15.5343928  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Luana",
+   location: { type: "Point", coordinates: [-43.3160809 ,-15.8052074  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Joana",
+   location: { type: "Point", coordinates: [ -43.310403, -15.7943479 ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Alberto e André",
+   location: { type: "Point", coordinates: [-43.3179225 ,-15.8057438  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Thalia",
+   location: { type: "Point", coordinates: [ -43.341919, -15.792172 ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Emilly Raissa",
+   location: { type: "Point", coordinates: [ -43.312611,-15.807591  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Savia",
+   location: { type: "Point", coordinates: [ -43.3223997,-15.7814025  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Dieyse",
+   location: { type: "Point", coordinates: [ -43.323578, -15.781380 ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Vitoria Ingred",
+   location: { type: "Point", coordinates: [-43.314893 ,-15.817489  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Joao Vitor",
+   location: { type: "Point", coordinates: [-43.306551 ,-15.8147075  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Michelle",
+   location: { type: "Point", coordinates: [-43.3123914 ,-15.8263155  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Maria Thais",
+   location: { type: "Point", coordinates: [ -43.0288791,-15.7377377  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Adrian",
+   location: { type: "Point", coordinates: [-43.304557 ,-15.811190  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Lara",
+   location: { type: "Point", coordinates: [-43.3153666 ,-15.8197566  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Camila",
+   location: { type: "Point", coordinates: [-43.2960149 , -15.8251198 ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Livia",
+   location: { type: "Point", coordinates: [-43.314748 ,-15.7953561  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Micaelle",
+   location: { type: "Point", coordinates: [-43.3148092 ,-15.8221044  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Flora",
+   location: { type: "Point", coordinates: [-43.3051085 , -15.8018395 ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Jade",
+   location: { type: "Point", coordinates: [-43.309728 ,-15.819087  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Sidney",
+   location: { type: "Point", coordinates: [ -43.3121088,-15.8036004  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Lavinia",
+   location: { type: "Point", coordinates: [ -43.3168428,-15.8045297  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Victoria Felipe",
+   location: { type: "Point", coordinates: [-43.300751 ,-15.818761  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Emille Beatriz",
+   location: { type: "Point", coordinates: [-43.3125666 ,-15.8039344  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Hellen",
+   location: { type: "Point", coordinates: [ -43.3165794,-15.7990549  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Joyce",
+   location: { type: "Point", coordinates: [-43.3087511 ,-15.8290764  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Amanda",
+   location: { type: "Point", coordinates: [ -43.314899,-15.802350  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Luan",
+   location: { type: "Point", coordinates: [ -43.3138138,-15.8007558  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Joao Gabriel",
+   location: { type: "Point", coordinates: [-43.3049733 ,-15.8489078  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Daniel",
+   location: { type: "Point", coordinates: [ -43.3084923,-15.8025955  ] },
+   category: "alunos"
+});
+db.alunos.insert( {
+   name: "Tom",
+   location: { type: "Point", coordinates: [-43.3090244 ,-15.802596  ] },
+   category: "alunos"
+});
+
+
